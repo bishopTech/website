16b577f03342a49f8a32f3d2a595601497b245f5
Chris Bishop <chris@bishop.tech>
2021-01-03 14:16:56 -0500

    updated bg color

--- gatsby/tailwind.config.js
+++ gatsby/tailwind.config.js
@@ -4,18 +4,11 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'darkest-blue': '#151A26',
+        'darkest-blue': '#0A1321',
         'darker-blue': '#202B45',
         'dark-blue': '#39496A',
         'light-blue': '#A4C9EE',
         'off-white': '#F0F7FE'
-
-        // lightest: '#F0F7FE',
-        // light: '#A4C9EE',
-        // DEFAULT: '#39496A',
-        // dark: '#202B45',
-        // darkest: '#151A26'
-
       }
     }
 
@@ -26,7 +19,7 @@ module.exports = {
   plugins: []
 }
 
-/* Dark Blue BG: #151A26 */
+/* Dark Blue BG: #151A26 or #0A1321 */
 /* Primary Dark Blue: #202B45 */
 /* Secondary Dark Blue: #39496A */
 /* Light Blue: #A4C9EE */
