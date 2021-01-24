e817e2b4c81b462460401dcc8cf50f0d4ae44c23
Chris Bishop <chris@bishop.tech>
2021-01-23 19:22:39 -0500

    Fixed the Where to start button

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -155,8 +155,8 @@ export default function index ({data: {post, image, bgImage, downIcon, personalI
         <h2 className='text-center text-lg'>
           If this your first time looking into the world of coding I suggest starting with any of these series:
         </h2>
-        <div className='mt-16'>
-          <div className='py-8 bg-gray-200 group transform transition rounded-full text-center shadow duration-300 hover:scale-110 hover:shadow-xl'>
+        <div className='mt-16 flex justify-center'>
+          <div className='py-8 px-32 bg-gray-200 group transform transition rounded-full text-center shadow duration-300 hover:scale-110 hover:shadow-xl'>
             <span className='text-3xl group-hover:hidden'>Coming Soon!</span>
             <span className='text-sm hidden transition transition duration-300 group-hover:block text-3xl'>really soon 🤞</span>
           </div>
