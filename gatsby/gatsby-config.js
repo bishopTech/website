906164f7c44d9fd8d00dd998c60bb6a843fc2c73
Chris Bishop <chris@bishop.tech>
2021-01-30 08:45:21 -0500

    added google analytics

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -50,6 +50,17 @@ module.exports = {
         ],
         display: 'swap'
       }
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'G-000SWE6S4N' // Google Analytics / GA
+          // 'AW-CONVERSION_ID', // Google Ads / Adwords / AW
+          // 'DC-FLOODIGHT_ID' // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ]
+      }
     }
   ]
 }
