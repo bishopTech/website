a90715ab9e788c13587ab792c2b8df772d33ed33
Chris Bishop <chris@bishop.tech>
2021-02-07 11:30:15 -0500

    fixed 404 page

--- gatsby/src/pages/404.js
+++ gatsby/src/pages/404.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Layout from '../components/layout/Layout'
 
-import OldSEO from '../components/sanity/oldSEO'
+import SEO from '../components/layout/SEO'
 
 const NotFoundPage = () => (
   <Layout>
-    <OldSEO title='404: Not found' />
+    <SEO title='404: Not found' />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
