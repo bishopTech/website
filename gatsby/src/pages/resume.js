e8a92651d6899011e8b5af813ff35f145d59577f
Chris Bishop <chris@bishop.tech>
2021-02-15 13:55:39 -0500

    added seo to resume page

--- gatsby/src/pages/resume.js
+++ gatsby/src/pages/resume.js
@@ -4,6 +4,7 @@ import Layout from '../components/layout/Layout'
 export default function resume () {
   return (
     <Layout>
+      <SEO title='Resume' />
       <h1 className='text-green-800'>Resume Page</h1>
     </Layout>
   )
